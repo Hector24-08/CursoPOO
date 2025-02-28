5ef3721ff696e9482ab60ccf4483e46e0ce803d7
Danielk10 <danielpdiamon@gmail.com>
2025-02-28 18:34:56 -0400

    Case numero 1

--- app/src/main/java/com/adrian/cursopoo/MainActivity.java
+++ app/src/main/java/com/adrian/cursopoo/MainActivity.java
@@ -1,27 +1,57 @@
-
 package com.adrian.cursopoo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import com.adrian.cursopoo.databinding.ActivityMainBinding;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(new View(this));
 
-        // Inflate and get instance of binding
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Creando o instanciqndo un objeto de la
+        // Clase Gato
 
-        // set content view to binding's root
-        setContentView(binding.getRoot());
+        Gato pepe = new Gato();
+        // Accediendo al atributo edad con el objeto
+        // pepe
+        pepe.edad = 23;
+
+        // Accediendo al metodo comer con el objeto
+        // pepe
+        pepe.comer();
     }
-    
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        this.binding = null;
+
+    // Definiendo o implementado la clase Gato
+
+    class Gato {
+
+        // Atributos o propiedades
+        // de la clase Gato
+        int edad = 5;
+        int peso = 4;
+        char sexo = 'M';
+        boolean vivo = true;
+
+        // Metodos de la clase Gato
+        void comer() {}
+
+        void dormir() {}
+
+        void mauyar() {}
+    }
+
+    class Planta {
+
+        // Atributos
+        int edad = 12;
+        int tamano = 34;
+
+        // Metodos
+        void dorrmir() {}
+
+        void crece() {}
     }
 }
