02151c6722e7b7466871fd29e9228a9cf1cd642f
Danielk10 <danielpdiamon@gmail.com>
2025-03-02 16:10:05 -0400

    Practica 1

---
+++ app/src/main/java/com/adrian/plantas/Vid.java
@@ -0,0 +1,34 @@
+package com.adrian.plantas;
+
+public class Vid {
+
+    // Atributos
+
+    private int numeroFlores = 1000;
+
+    protected float tiempoCosecha = 3.4;
+
+    public boolean planta = true;
+
+    boolean comestible = true;
+
+    // Metodo constructor
+
+    public Vid() {
+        
+    }
+
+    // Metodos
+
+    void crece() {
+        
+    }
+
+    public void duerme() {
+        
+    }
+
+    public void respira() {
+        
+    }
+}
